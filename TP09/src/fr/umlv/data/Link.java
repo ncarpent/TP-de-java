4928b2c4dc02203b8ffd65b20418d8c0b816ac91
ncarpent <106631411+ncarpent@users.noreply.github.com>
2022-06-11 18:33:22 +0200

    Update Link.java

--- TP09/src/fr/umlv/data/Link.java
+++ TP09/src/fr/umlv/data/Link.java
@@ -6,27 +6,12 @@ record Link<E> (E value, Link<E> next)
 	
 }
 
-/*
-record Link(Object value, Link next)
-{
-	
-	public static void main(String[] args)
-	{
-		Link l1 = new Link(144, null);
-		Link l2 = new Link(13, l1);
-		System.out.println(l2);
-	}
-	
-}
-*/
+
 
 /* 
  * Exercice 1 :
  * 
- * Q1/ Le record Link doit garder une visibilité par défaut.
- * 		Il sera disponible dans les fichiers du même package.
+ * Q1/ Le record Link doit garder une visibilitÃ© par dÃ©faut.
+ * 		Il sera disponible dans les fichiers du mÃªme package.
  * 
- * Q2/ javac fr.umlv.data.Link.java
- * 		java fr.umlv.data.Link
- * 		ok ???
  */
