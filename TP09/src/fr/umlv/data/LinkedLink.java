a73ee948ce9c7d0a80a405e4cbf60d9551a12b76
ncarpent <106631411+ncarpent@users.noreply.github.com>
2022-06-11 18:47:07 +0200

    Update LinkedLink.java

--- TP09/src/fr/umlv/data/LinkedLink.java
+++ TP09/src/fr/umlv/data/LinkedLink.java
@@ -36,8 +36,8 @@ public class LinkedLink<E>
 	
 	public E get (int index)
 	{
-		if (index < 0) {throw new IndexOutOfBoundsException("indice négatif");}
-		if (index >= size) {throw new IndexOutOfBoundsException("indice supérieur à la taille de la liste");}
+		if (index < 0) {throw new IndexOutOfBoundsException("indice nÃ©gatif");}
+		if (index >= size) {throw new IndexOutOfBoundsException("indice supÃ©rieur Ã  la taille de la liste");}
 		Link<E> linkit = first;
 		for (int i=0 ; i<index ; i++)
 		{
@@ -46,9 +46,7 @@ public class LinkedLink<E>
 		return linkit.value();
 	}
 	
-	public boolean contains (Object o) //o doit être de type Object ??? car on peut demander
-	// théoriquement pour n'importe quel objet ?? à tester...
-	// si l'on ne connaît pas exactement le type de la liste ?...
+	public boolean contains (Object o)
 	{
 		Link<E> linkit = first;
 		for (int i=0 ; i<size ; i++, linkit = linkit.next())
@@ -62,53 +60,3 @@ public class LinkedLink<E>
 	}
 	
 }
-
-/*
-public class LinkedLink
-{
-	private Link first = null;
-	private int size = 0;
-	
-	
-	
-	public void add (Object value)
-	{
-		first = new Link(value, first);
-		size++;
-	}
-	
-	@Override
-	public String toString ()
-	{
-		StringBuilder retour = new StringBuilder("[");
-		Link l = first;
-		if (l == null)
-		{
-			retour.append(" ]");
-		}
-		else
-		{
-			while (l.next() != null)
-			{
-				retour.append(l.value() + ", ");
-				l = l.next();
-			}
-			retour.append(l.value() + "]");
-		}
-		return retour.toString();
-	}
-	
-	public Object get (int index)
-	{
-		if (index < 0) {throw new IndexOutOfBoundsException("indice négatif");}
-		if (index >= size) {throw new IndexOutOfBoundsException("indice supérieur à la taille de la liste");}
-		Link linkit = first;
-		for (int i=0 ; i<index ; i++)
-		{
-			linkit = linkit.next();
-		}
-		return linkit.value();
-	}
-	
-}
-*/
