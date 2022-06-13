5f95ab11fdd9fde00cffb5e66f85061c6790ef4f
ncarpent <106631411+ncarpent@users.noreply.github.com>
2022-06-13 19:09:25 +0200

    Update Expr.java

--- TP07/src/fr/umlv/calc/Expr.java
+++ TP07/src/fr/umlv/calc/Expr.java
@@ -1,7 +1,6 @@
 package fr.umlv.calc;
 
 import java.util.Iterator;
-//import java.util.Scanner;
 
 public sealed interface Expr //permits Expr.Add, Expr.Sub, Expr.Mul, Expr.Value//, Expr.Binop
 {
@@ -10,7 +9,7 @@ public sealed interface Expr //permits Expr.Add, Expr.Sub, Expr.Mul, Expr.Value/
 	
 	public String toString ();
 	
-	public static Expr parse (Iterator<String> sc) // Ne prends pas une list !!!!
+	public static Expr parse (Iterator<String> sc)
 	{
 		String next = sc.next();
 		return switch (next)
@@ -107,62 +106,18 @@ public sealed interface Expr //permits Expr.Add, Expr.Sub, Expr.Mul, Expr.Value/
 	}
 	
 	/*
-	public static void main(String[] args)
-	{
-		
-		Expr expression = new Add(new Value(2), new Value(3));
-		Expr expression2 = new Sub(new Mul(new Value(2), new Value(3)), new Value(4));
-		System.out.println(expression.eval());
-		System.out.println(expression2.eval());
-		System.out.println(Integer.parseInt("007"));
-		Scanner sc = new Scanner("- + 2 3 4");
-		Expr expression3 = Expr.parse(sc);
-		System.out.println(expression3.eval());
-	}
-	*/
-	
-	/* méthode parse définie pour un Scanner :
-	public static Expr parse (Scanner sc)
-	{
-		if (sc.hasNextInt())
-		{
-			return new Value(sc.nextInt());
-		}
-		else
-		{
-			switch (sc.next())
-			{
-			case "+" :
-			{
-				return new Add(parse(sc), parse(sc));
-			}
-			case "-" :
-			{
-				return new Sub(parse(sc), parse(sc));
-			}
-			case "*" :
-			{
-				return new Mul(parse(sc), parse(sc));
-			}
-			default : throw new IllegalArgumentException("symbole non reconnu.");
-			}
-		}
-	}
-	*/
-	
-	/*
 	 * Q4 : Il faut fermer l'interface Expr.
 	 * 
-	 * Q5 : c'est intéressant car on peut désormais fermer l'interface sans spécifier les types qui
-	 * 		sont autorisés (car ceux-ci sont présents dans l'interface).
+	 * Q5 : c'est intÃ©ressant car on peut dÃ©sormais fermer l'interface sans spÃ©cifier les types qui
+	 * 		sont autorisÃ©s (car ceux-ci sont prÃ©sents dans l'interface).
 	 * 
-	 * Q9 : Binop doit être une interface, pour pouvoir subvenir à toutes les attentes de ce nouveau
+	 * Q9 : Binop doit Ãªtre une interface, pour pouvoir subvenir Ã  toutes les attentes de ce nouveau
 	 * 		type :
-	 * 		_ On peut donc définir certaines méthodes qui étaient abstraites dans Expr (comme eval, qui
-	 * 		est le but de la création de BinOp).
-	 * 		_ On peut garder certaines méthodes abstraites (comme toString, qui dépendra de chaque
+	 * 		_ On peut donc dÃ©finir certaines mÃ©thodes qui Ã©taient abstraites dans Expr (comme eval, qui
+	 * 		est le but de la crÃ©ation de BinOp).
+	 * 		_ On peut garder certaines mÃ©thodes abstraites (comme toString, qui dÃ©pendra de chaque
 	 * 		type).
-	 * 		_ Et on peut générer de nouvelles méthodes abstraites (comme op, qui spécifiera le
+	 * 		_ Et on peut gÃ©nÃ©rer de nouvelles mÃ©thodes abstraites (comme op, qui spÃ©cifiera le
 	 * 		fonctionnement de eval dans chaque sous-type)
 	 * 
 	 */
